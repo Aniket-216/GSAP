c2ba52392353c34a5d830dba5e5e1d9e1900ec44
aniket216 <aniketshinde216@gmail.com>
2024-12-27 17:39:40 +0530

    Part-08 project-05

--- part-08/Project-05/script.js
+++ part-08/Project-05/script.js
@@ -1,25 +1,98 @@
-window.addEventListener("wheel", function (event) {
-    if (event.deltaY > 0) {
-        gsap.to(".marque", {
-            transform: "translateX(-200%)",
-            duration: 4,
-            repeat: -1,
-            ease: "none",
-        });
+function Page1Animation() {
+    var tl = gsap.timeline();
 
-        gsap.to(".marque img", {
-            rotate: 180,
-        });
-    } else {
-        gsap.to(".marque", {
-            transform: "translateX(0%)",
-            duration: 4,
-            repeat: -1,
-            ease: "none",
-        });
+    tl.from("nav h1, nav h4, nav button", {
+        y: -40,
+        opacity: 0,
+        duration: 0.7,
+        delay: 0.5,
+        stagger: 0.25,
+    });
 
-        gsap.to(".marque img", {
-            rotate: 0,
-        });
-    }
+    tl.from(".center-part1 h1", {
+        x: -300,
+        opacity: 0,
+        duration: 0.5,
+    });
+    tl.from(".center-part1 p", {
+        x: -100,
+        opacity: 0,
+        duration: 0.4,
+    });
+    tl.from(".center-part1 button", {
+        opacity: 0,
+        duration: 0.4,
+    });
+
+    tl.from(
+        ".center-part2 img",
+        {
+            opacity: 0,
+            duration: 0.5,
+        },
+        "-=0.5"
+    );
+
+    tl.from(".section1bottom img", {
+        opacity: 0,
+        y: 30,
+        stagger: 0.15,
+        duration: 0.5,
+    });
+}
+
+Page1Animation();
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".services h3",
+        scroller: "body",
+        markers: true,
+        start: "top 50%",
+        end: "top -50%",
+        scrub: 2,
+    },
 });
+
+tl2.from(".services", {
+    y: 30,
+    opacity: 0,
+    duration: 0.5,
+});
+
+tl2.from(
+    ".elem.line1.left",
+    {
+        x: -300,
+        opacity: 0,
+        duration: 0.5,
+    },
+    "anim1"
+);
+tl2.from(
+    ".elem.line1.right",
+    {
+        x: 300,
+        opacity: 0,
+        duration: 0.5,
+    },
+    "anim1"
+);
+tl2.from(
+    ".elem.line2.left",
+    {
+        x: -300,
+        opacity: 0,
+        duration: 0.5,
+    },
+    "anim2"
+);
+tl2.from(
+    ".elem.line2.right",
+    {
+        x: 300,
+        opacity: 0,
+        duration: 0.5,
+    },
+    "anim2"
+);
